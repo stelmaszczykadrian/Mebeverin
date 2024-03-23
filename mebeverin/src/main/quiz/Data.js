c578a493d60da13ddb640b87015e21d35226a665
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2024-03-23 14:01:51 +0100

    Create Quiz component

---
+++ mebeverin/src/main/quiz/Data.js
@@ -0,0 +1,28 @@
+import {blueColor, greenColor, orangeColor, purpleColor} from "../../header/Data";
+
+export const quizData = [
+    {
+        color: greenColor,
+        number: "1.",
+        description: 'Odpowiedz na 3 pytania o czynnościowe zaburzenia jelit i meweberyny.',
+
+    },
+    {
+        color: orangeColor,
+        number: "2.",
+        description: 'Tylko jedna odpowiedź jest prawidłowa.',
+
+    },
+    {
+        color: purpleColor,
+        number: "3.",
+        description: 'Po zaznaczeniu odpowiedz i kliknięciu DALEJ nie ma możliwości powrotu do wcześniejszego pytania.',
+
+    },
+    {
+        color: blueColor,
+        number: "4.",
+        description: 'Wyślij formularz zgłoszeniowy do quizu.',
+
+    }
+];
---
+++ mebeverin/src/main/quiz/Quiz.jsx
@@ -0,0 +1,55 @@
+import './Quiz.css';
+import {quizData} from "./Data";
+
+const QuizIcon = ({color, number}) => (
+    <div className="quiz-icon" style={{backgroundColor: color}}>
+        <div className="quiz-number">{number}</div>
+    </div>
+);
+
+const QuizItem = ({color, number, description}) => (
+    <div className="quiz-rule">
+        <QuizIcon color={color} number={number}/>
+        <div className="quiz-rule-description">{description}</div>
+    </div>
+);
+
+export default function Quiz() {
+    return (
+        <div className="quiz-container" id="quiz">
+            <div className="quiz-info">
+                <div className="quiz-content">
+                    <div className="prize-container">
+                        <div className="quiz-title">QUIZ</div>
+                        <div className="quiz-subtitle">Odpowiedz na 3 pytania na temat meweberyny.</div>
+                        <div className="quiz-divider"></div>
+                        <div className="quiz-second-subtitle">Sprawdź swoją wiedzę i zawalcz o nagrodę w quizie!</div>
+                    </div>
+                    <div className="quiz-reward">
+                        <div className="reward-label">Do wygrania:</div>
+                        <div className="reward-icon">
+                            <div className="reward-circle">
+                                <span className="reward-amount">50x</span>
+                            </div>
+                        </div>
+                        <div className="reward-details">Opis nagrody</div>
+                    </div>
+                </div>
+            </div>
+            <div className="quiz-rules">
+                <div className="quiz-rules-title">Zasady quizu:</div>
+                <div className="quiz-rules-container">
+                    {quizData.map((quiz, index) => (
+                        <QuizItem
+                            key={index}
+                            color={quiz.color}
+                            number={quiz.number}
+                            description={quiz.description}
+                        />
+                    ))}
+                </div>
+            </div>
+            <button className="quiz-button">ROZPOCZNIJ QUIZ</button>
+        </div>
+    );
+}
