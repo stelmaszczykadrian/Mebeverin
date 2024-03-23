e72f95d10f139e5fb6d44c8212407d2152ee83a2
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2024-03-23 14:00:47 +0100

    Create Video component

---
+++ mebeverin/src/main/video/Data.js
@@ -0,0 +1,28 @@
+import ibsTherapy from "../../assets/IBSTeraphy.png";
+
+export const videos = [
+    {
+        name: "VIDEO WYKŁAD",
+        title: 'Skuteczna terapia IBS',
+        subtitle: 'dr n. med. Artur Gradowski',
+        image: ibsTherapy,
+        description: 'Skuteczne leczenie bólów brzucha stanowi wyzwanie w gabinetach POZ i specjalistów. Jak przekonać pacjenta do stosowania się do zaleceń lekarskich?',
+        youtubeLink: 'https://www.youtube.com/watch?v=SV9d-Hfe-vk&t=1s'
+    },
+    {
+        name: "VIDEO WYKŁAD 2",
+        title: 'Skuteczna terapia IBS',
+        subtitle: 'dr n. med. Artur Gradowski',
+        image: ibsTherapy,
+        description: 'Skuteczne leczenie bólów brzucha stanowi wyzwanie w gabinetach POZ i specjalistów. Jak przekonać pacjenta do stosowania się do zaleceń lekarskich?',
+        youtubeLink: 'https://www.youtube.com/watch?v=SV9d-Hfe-vk&t=1s'
+    },
+    {
+        name: "VIDEO WYKŁAD 3",
+        title: 'Skuteczna terapia IBS',
+        subtitle: 'dr n. med. Artur Gradowski',
+        image: ibsTherapy,
+        description: 'Skuteczne leczenie bólów brzucha stanowi wyzwanie w gabinetach POZ i specjalistów. Jak przekonać pacjenta do stosowania się do zaleceń lekarskich?',
+        youtubeLink: 'https://www.youtube.com/watch?v=SV9d-Hfe-vk&t=1s'
+    },
+];
---
+++ mebeverin/src/main/video/Video.jsx
@@ -0,0 +1,62 @@
+import './Video.css';
+import React, {useState} from "react";
+import arrowRightSvg from "../../assets/svg/arrow-right.svg";
+import polygonRight from '../../assets/svg/polygon-right.svg';
+import {videos} from "./Data";
+
+export default function Video() {
+    const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+    const handleVideoClick = (index) => {
+        setCurrentVideoIndex(index);
+    };
+
+    const handleNextVideo = () => {
+        setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
+    };
+
+    const currentVideo = videos[currentVideoIndex];
+
+    return (
+        <div className="video" id="video">
+            <div className="video-header">VIDEO</div>
+            <div style={{display: 'flex', justifyContent: 'flex-start'}}>
+                <div className="video-list">
+                    {videos.map((podcast, index) => (
+                        <div
+                            key={index}
+                            className={`video-list-item ${index === currentVideoIndex ? 'active' : ''}`}
+                            onClick={() => handleVideoClick(index)}
+                        >
+                            {podcast.name}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div style={{display: 'flex'}}>
+                <div className="video-image-container">
+                    <div className="video-circle">
+                        <img src={polygonRight} alt="Polygon right"></img>
+                    </div>
+                    <img src={currentVideo.image} alt="Video Thumbnail"/>
+                </div>
+                <div className="video-content">
+                    <div className="video-title">{currentVideo.title}</div>
+                    <div className="video-subtitle">{currentVideo.subtitle}</div>
+                    <div className="video-divider"></div>
+                    <div style={{display: 'flex'}}>
+                        <div className="video-description">{currentVideo.description}</div>
+                        <div style={{marginLeft: '1rem'}}>
+                            <a className="video-arrow-right" onClick={handleNextVideo}>
+                                <img src={arrowRightSvg} alt="Arrow Right"/>
+                            </a>
+                        </div>
+                    </div>
+                    <button className="video-button"
+                            onClick={() => window.location.href = currentVideo.youtubeLink}>OGLĄDAM
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
