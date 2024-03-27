05eea47d81c1c6cbefcbde0ded5519efaee6de70
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2024-03-27 18:00:11 +0100

    Adjustment Video section view for large, medium, and small screens

--- mebeverin/src/main/video/Video.jsx
+++ mebeverin/src/main/video/Video.jsx
@@ -2,7 +2,7 @@ import './Video.css';
 import React, {useState} from "react";
 import arrowRightSvg from "../../assets/svg/arrow-right.svg";
 import polygonRight from '../../assets/svg/polygon-right.svg';
-import {videos} from "./Data";
+import {videos} from "./VideoData";
 
 export default function Video() {
     const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
@@ -18,43 +18,45 @@ export default function Video() {
     const currentVideo = videos[currentVideoIndex];
 
     return (
-        <div className="video" id="video">
-            <div className="video-header">VIDEO</div>
-            <div style={{display: 'flex', justifyContent: 'flex-start'}}>
-                <div className="video-list">
-                    {videos.map((podcast, index) => (
-                        <div
-                            key={index}
-                            className={`video-list-item ${index === currentVideoIndex ? 'active' : ''}`}
-                            onClick={() => handleVideoClick(index)}
-                        >
-                            {podcast.name}
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <div style={{display: 'flex'}}>
-                <div className="video-image-container">
-                    <div className="video-circle">
-                        <img src={polygonRight} alt="Polygon right"></img>
+        <div id="video">
+            <div className="video-wrapper text-center align-items-center justify-content-center container">
+                <div className="video-header">VIDEO</div>
+                <div className="video-list-item-container">
+                    <div className="video-list">
+                        {videos.map((podcast, index) => (
+                            <div
+                                key={index}
+                                className={`video-list-item ${index === currentVideoIndex ? 'active' : ''}`}
+                                onClick={() => handleVideoClick(index)}
+                            >
+                                {podcast.name}
+                            </div>
+                        ))}
                     </div>
-                    <img src={currentVideo.image} alt="Video Thumbnail"/>
                 </div>
-                <div className="video-content">
-                    <div className="video-title">{currentVideo.title}</div>
-                    <div className="video-subtitle">{currentVideo.subtitle}</div>
-                    <div className="video-divider"></div>
-                    <div style={{display: 'flex'}}>
-                        <div className="video-description">{currentVideo.description}</div>
-                        <div style={{marginLeft: '1rem'}}>
-                            <a className="video-arrow-right" onClick={handleNextVideo}>
-                                <img src={arrowRightSvg} alt="Arrow Right"/>
-                            </a>
+                <div className="video-container">
+                    <div className="video-image-container">
+                        <div className="video-circle">
+                            <img src={polygonRight} alt="Polygon right"></img>
                         </div>
+                        <img className="video-image" src={currentVideo.image} alt="Video Thumbnail"/>
                     </div>
-                    <button className="video-button"
-                            onClick={() => window.location.href = currentVideo.youtubeLink}>OGLĄDAM
-                    </button>
+                    <div className="video-content">
+                        <div className="video-title">{currentVideo.title}</div>
+                        <div className="video-subtitle">{currentVideo.subtitle}</div>
+                        <div className="video-divider"></div>
+                        <div style={{display: 'flex'}}>
+                            <div className="video-description">{currentVideo.description}</div>
+                            <div style={{marginLeft: '1rem'}}>
+                                <a className="video-arrow-right" onClick={handleNextVideo}>
+                                    <img className="video-arrow-right-img" src={arrowRightSvg} alt="Arrow Right"/>
+                                </a>
+                            </div>
+                        </div>
+                        <button className="video-button"
+                                onClick={() => window.location.href = currentVideo.youtubeLink}>OGLĄDAM
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
